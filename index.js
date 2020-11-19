2cae91464759318ed596d1997dcbb2ed68e7c7a0
Loic Chambost <chambost.loic@gmail.com>
2020-11-19 11:08:13 +0100

    apply standard

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import DataStore from 'nedb'
 const PORT = 8000
 
 // BDD
-const db = new DataStore({ filename: "perso" })
+const db = new DataStore({ filename: 'perso' })
 db.loadDatabase()
 
 // demarrage d'express
@@ -15,50 +15,44 @@ app.use(express.json())
 
 // Create
 app.post('/api/perso', (req, res) => {
-    console.log(req.body)
-    db.insert(req.body)
-    res.send(req.body)
+  console.log(req.body)
+  db.insert(req.body)
+  res.send(req.body)
 })
 
 // Read ALL
 app.get('/api/perso', (req, res) => {
-    db.find({}, (err, docs) => {
-        if (err) console.log(err)
+  db.find({}, (err, docs) => {
+    if (err) console.log(err)
 
-        res.send(docs)
-    })
+    res.send(docs)
+  })
 })
 
 // Read ONE
 app.get('/api/perso/:id', (req, res) => {
-    db.find({ _id: req.params.id }, (err, docs) => {
-        if (err) console.log(err)
+  db.find({ _id: req.params.id }, (err, docs) => {
+    if (err) console.log(err)
 
-        res.send(docs)
-    })
+    res.send(docs)
+  })
 })
 
 // Update
 app.patch('/api/perso/:id', (req, res) => {
-    db.update({ _id: req.params.id }, { $set: { ...req.body } })
-    res.send(req.body)
+  db.update({ _id: req.params.id }, { $set: { ...req.body } })
+  res.send(req.body)
 })
 
 // Delete
 app.delete('/api/perso/:id', (req, res) => {
-    db.remove({ _id: req.params.id }, (err, docs) => {
-        if (err) console.log(err)
+  db.remove({ _id: req.params.id }, (err, docs) => {
+    if (err) console.log(err)
 
-        res.send(docs)
-    })
+    res.send(docs)
+  })
 })
 
-
-
-
-
-
-
 app.listen(PORT, () => {
-    console.log(`Serveur lancé sur le port ${PORT}`)
+  console.log(`Serveur lancé sur le port ${PORT}`)
 })
